ef4c49ce6b2706a1b85112302279cfdd7a226297
Nguyen The Manh <manhnt0210@gmail.com>
2019-08-08 11:11:11 +0700

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "signup_suc"
+      redirect_to root_url
+    else
+      flash[:danger] = t "danger"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :address, :phone, :password,
+      :password_confirmation
+  end
+end
