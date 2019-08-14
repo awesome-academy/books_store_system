1e5ddb5ab137a360666170c402013eb556b64895
Nguyen The Manh <manhnt0210@gmail.com>
2019-08-14 22:44:32 +0700

    review product

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -28,3 +28,14 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+$(document).on('click', '.rating .fa-star', function () {
+  let rate = $(this).attr('data-rate');
+  let star_dom = $('.rating .fa-star');
+  star_dom.removeClass('active');
+  for (let i = 0; i < rate; i++) {
+    star_dom.eq(i).addClass('active');
+  }
+
+  $('#review_rate').val(rate);
+});
