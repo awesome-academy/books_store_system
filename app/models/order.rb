0559361e24a98801e914869eace00014fcae9b77
Nguyen The Manh <manhnt0210@gmail.com>
2019-08-29 14:29:13 +0700

    update order

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,6 @@ class Order < ApplicationRecord
   enum deliver_status: {processing: 0, cancel: 1, success: 2}
 
   ORDER_PARAMS = %i(receiver phone address total_price).freeze
-
   validates :receiver, presence: true
   validates :address, presence: true
   validates :phone, presence: true
